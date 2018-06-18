27e40e43b8156ecdab87a39ab919cad05828f9a2
Patrick McCarthy <p.t.mccarthy@gmail.com>
2018-06-17 22:00:01 -0700

    Add bodyParser and test a post/create

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,9 +7,22 @@ const routes = new Router();
 
 routes
   .get('/', async (ctx) => {
+    logger.info('GET /');
     const { id } = ctx.params;
     const { rows } = await db.query('SELECT * from cars', []);
     ctx.body = rows;
-  });
+  })
+
+  .post('/create', async (ctx) => {
+    logger.info('POST /create');
+    const name = ctx.request.body.name;
+
+    if (name) {
+      const res = await db.query('INSERT INTO cars VALUES ($1)', [name]);
+      ctx.status = 200;
+    } else {
+      ctx.status = 400;
+    }
+  })
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
+import * as bodyParser from 'koa-bodyparser';
 
-import { db } from './db/db';
 import logger from './logger';
 import routes from './routes/routes';
 
@@ -10,6 +10,7 @@ const PORT = 3000;
 const koa = new Koa();
 const router = new Router();
 
+koa.use(bodyParser());
 koa.use(routes.routes());
 koa.listen(PORT);
-logger.info(`Koa listening on port ${3000}`);
+logger.info(`Koa listening on port ${PORT}`);
