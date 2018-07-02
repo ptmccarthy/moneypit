b3787007cf90f88eb7a2f736faf2d0095750e37b
Patrick McCarthy <p.t.mccarthy@gmail.com>
2018-07-02 13:30:07 -0700

    Create user entity

---
+++ src/entity/user.ts
@@ -0,0 +1,18 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column()
+  public firstName: string;
+
+  @Column()
+  public lastName: string;
+
+  @Column()
+  public age: number;
+
+}
